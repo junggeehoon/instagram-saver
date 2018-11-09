0776344dd790d4105115941e580ab0ec2fc6e5b7
junggeehoon <junggeehoon@gmail.com>
2018-11-10 01:45:44 +0900

    late light

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const download = require('image-downloader');
 
 // Relative path to directory to download images
 const IMAGE_DIRECTORY = './img';
+const delay = 300;
+let count = 0;
+
+const extractImage = () => {
+  const elements = document.querySelectorAll('div.KL4Bh > img');
+  return [...elements].map(el => el.src);
+}
+
+const extractLink = () => {
+  const elements = document.querySelectorAll('div._2z6nI > article > div > div > div > div > a');
+  return [...elements].map(el => el.href);
+}
 
 const downloadImg = async (options = {}) => {
   try {
@@ -15,28 +27,44 @@ const downloadImg = async (options = {}) => {
   }
 }
 
-const extractItems = () => {
-  const elements = document.querySelectorAll('div.KL4Bh > img');
-  return [...elements].map(el => el.src);
+const scrapPostImage = async (links) => {
+  try {
+    for (let i = 0; i < links.length; i++) {
+      const browser = await puppeteer.launch({headless: false});
+      const page = await browser.newPage();
+      page.setViewport({ width: 1080, height: 720 });
+      await page.goto(links[i]);
+      let nextBtn = await page.evaluate("document.querySelectorAll('.coreSpriteRightChevron')");
+      while (nextBtn === undefined) {
+        const imgs = await page.evaluate(extractImage);
+        await Promise.all(imgs.map(async file => {
+          await downloadImg({
+            url: file,
+            dest: IMAGE_DIRECTORY 
+          });
+        }));
+        count += imgs.length;
+        await page.click('.coreSpriteRightChevron');
+        await page.waitFor(delay);
+        nextBtn = await page.evaluate("document.querySelectorAll('.coreSpriteRightChevron')");
+      }
+      browser.close();
+    }
+    return;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-const scrapeInfiniteScrollItems = async (page, extractItems, scrollDelay) => {
+const scrapeInfiniteScrollItems = async page => {
   try {
-    let previousHeight;
-    let count = 0;
-    while (count < 330) {
-      const imgs = await page.evaluate(extractItems);
-      await Promise.all(imgs.map(async file => {
-        await downloadImg({
-          url: file,
-          dest: IMAGE_DIRECTORY 
-        });
-      }));
+    while (count < 600) {
+      const links = await page.evaluate(extractLink);
+      await scrapPostImage(links);
       previousHeight = await page.evaluate('document.body.scrollHeight');
       await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
       await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-      await page.waitFor(scrollDelay);
-      count += imgs.length;
+      await page.waitFor(delay);
     }
     console.log(`👌  Done -- downloaded \x1b[36m${count}\x1b[0m album covers!`);
   } catch(err) {
@@ -53,13 +81,12 @@ const scrapeImgUrls = async () => {
     page.setViewport({ width: 1080, height: 720 });
 
     console.log('🌎  Visiting web page...');
-    await page.goto('https://www.instagram.com/yoona__lim/');
+    await page.goto('https://www.instagram.com/h.young_j/');
 
     console.log('⛏  Scraping image URLs...');
-    await scrapeInfiniteScrollItems(page, extractItems, scrollDelay = 200);
+    await scrapeInfiniteScrollItems(page);
 
-    browser.close();
-    return;
+    return browser.close();
   }
   catch(err) {
     console.log(err);
